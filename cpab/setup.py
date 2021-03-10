a1ae93c06e7886a13b83a7071d791ef79e8c4819
imartinezl <inigomlap@gmail.com>
2021-03-10 23:38:05 +0100

    Setup.py format

--- cpab/setup.py
+++ cpab/setup.py
@@ -16,58 +16,63 @@ URL = "https://github.com/imartinezl/diffeomorphism"
 AUTHOR = "Iñigo Martinez"
 AUTHOR_EMAIL = "inigomlap@gmail.com"
 CLASSIFIERS = [
-    'Development Status :: 2 - Pre-Alpha',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: MIT License',
-    'Natural Language :: English',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3.9',
+    "Development Status :: 2 - Pre-Alpha",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Natural Language :: English",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
 ]
 ENTRY_POINTS = {
-    'console_scripts': [
+    "console_scripts": [
         #'tsclust=tsclust:main',
     ],
 }
 PROJECT_URLS = {
-    'Bug Reports': URL + '/issues',
-    'Documentation': 'https://cpab.readthedocs.io',
-    'Source Code': URL,
+    "Bug Reports": URL + "/issues",
+    "Documentation": "https://cpab.readthedocs.io",
+    "Source Code": URL,
 }
-REQUIRES_PYTHON = '>=3.5, <4'
+REQUIRES_PYTHON = ">=3.5, <4"
 EXTRAS_REQUIRE = {}
-KEYWORDS = ["diffeomorphisms", "tessellations", "transformations", "continuous piecewise-affine velocity fields"]
+KEYWORDS = [
+    "diffeomorphisms",
+    "tessellations",
+    "transformations",
+    "continuous piecewise-affine velocity fields",
+]
 LICENSE = "MIT license"
 TEST_SUITE = "tests"
-REQUIREMENTS = ['numpy', 'torch', 'scipy', 'matplotlib']
+REQUIREMENTS = ["numpy", "torch", "scipy", "matplotlib"]
 SETUP_REQUIREMENTS = []
-TEST_REQUIREMENTS = ['pytest', 'pytest-cov']
+TEST_REQUIREMENTS = ["pytest", "pytest-cov"]
 
 setuptools.setup(
-    author = AUTHOR,
-    author_email = AUTHOR_EMAIL,
-    classifiers = CLASSIFIERS,
-    description = DESCRIPTION,
-    entry_points = ENTRY_POINTS,
-    extras_require = EXTRAS_REQUIRE,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    classifiers=CLASSIFIERS,
+    description=DESCRIPTION,
+    entry_points=ENTRY_POINTS,
+    extras_require=EXTRAS_REQUIRE,
     # ext_modules = [module1]
-    include_package_data = False,
-    install_requires = REQUIREMENTS,
-    keywords = KEYWORDS,
-    license = LICENSE,
-    long_description = README,
-    name = NAME,
+    include_package_data=False,
+    install_requires=REQUIREMENTS,
+    keywords=KEYWORDS,
+    license=LICENSE,
+    long_description=README,
+    name=NAME,
     package_data={},
     # packages = setuptools.find_packages(),
-    project_urls = PROJECT_URLS,
-    python_requires = REQUIRES_PYTHON,
-    setup_requires = SETUP_REQUIREMENTS,
-    test_suite = TEST_SUITE,
-    tests_require = TEST_REQUIREMENTS,
-    url = URL,
+    project_urls=PROJECT_URLS,
+    python_requires=REQUIRES_PYTHON,
+    setup_requires=SETUP_REQUIREMENTS,
+    test_suite=TEST_SUITE,
+    tests_require=TEST_REQUIREMENTS,
+    url=URL,
     # version = VERSION,
-    zip_safe = False,
+    zip_safe=False,
 )
