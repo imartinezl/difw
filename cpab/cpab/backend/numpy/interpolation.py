ae7c5634a0cb27bb59f4d59066c876fbd07681af
imartinezl <inigomlap@gmail.com>
2021-03-15 17:16:02 +0100

    Numpy backend: refactor from functions to interpolate and transform

---
+++ cpab/cpab/backend/numpy/interpolation.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+
+# TODO: review channels dimension and overall data dimension
+# for sure, we need two dimensions:
+#   1. One for batch_size
+#   2. Another for time series length
+# not sure why we need a third one
+
+
+def interpolate(data, grid, outsize):
+    # Problem size
+    n_batch, width, n_channels = data.shape
+
+    # Extract points
+    x = grid.flatten()
+
+    # Scale to domain
+    x = x * (width - 1)
+
+    # Do sampling
+    x0 = np.floor(x).astype(np.int32)
+    x1 = x0 + 1
+
+    # Clip values
+    x0 = np.clip(x0, 0, width - 1)
+    x1 = np.clip(x1, 0, width - 1)
+
+    # Batch effect
+    r = np.arange(n_batch).repeat(outsize)
+
+    # Index
+    y0 = data[r, x0, :]
+    y1 = data[r, x1, :]
+
+    # Interpolation weights
+    xd = (x - x0.astype(np.float32)).reshape((-1, 1))
+
+    # Do interpolation
+    y = y0 * (1 - xd) + y1 * xd
+    
+    newdata = np.reshape(y, (n_batch, outsize, n_channels))
+    return newdata
