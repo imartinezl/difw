d6963986ed1b9de32e89b7cfb4a2b348d3b9c482
imartinezl <inigomlap@gmail.com>
2021-03-29 14:09:16 +0200

    modes class

--- cpab/cpab/core/utility.py
+++ cpab/cpab/core/utility.py
@@ -24,3 +24,23 @@ class Parameters:
 def get_dir(file):
     """ Get directory of the input file """
     return os.path.dirname(os.path.realpath(file))
+
+
+# %%
+
+# TODO: review name: modes, method, 
+class modes:
+    closed_form = "closed_form"
+    numeric = "numeric"
+
+    @staticmethod
+    def check_mode(mode):
+        if mode is not None:
+            assert mode in [modes.closed_form, modes.numeric], "Unknown mode, choose between " + Modes.closed_form + " or " + Modes.numeric
+
+    @staticmethod
+    def default(mode=None):
+        if mode is not None:
+            return mode
+        else:
+            return modes.closed_form
