e2364853b77e92211bbe3805732da4e78b827953
imartinezl <inigomlap@gmail.com>
2021-03-29 15:57:32 +0200

    Pytorch transformer fast: rename and organize

--- cpab/cpab/backend/pytorch/transformer.cpp
+++ cpab/cpab/backend/pytorch/transformer.cpp
@@ -2,8 +2,75 @@
 #include "../../core/cpab.h"
 #include <iostream>
 
-at::Tensor get_affine(at::Tensor B, at::Tensor theta){
+// FUNCTIONS
+
+at::Tensor torch_get_affine(at::Tensor B, at::Tensor theta){
     return at::matmul(B, theta);//.reshape({-1,2});
+}
+
+at::Tensor torch_get_cell(at::Tensor points, const float xmin, const float xmax, const int nc){
+    const int n_points = points.size(0);
+    float* x = points.data_ptr<float>();
+
+    auto output = torch::zeros({n_points}, at::kCPU);
+    auto newpoints = output.data_ptr<float>();
+
+    for(int i = 0; i < n_points; i++) {
+        newpoints[i] = get_cell(x[i], xmin, xmax, nc);
+    }
+    return output;
+}
+
+at::Tensor torch_get_velocity(at::Tensor points, at::Tensor theta, at::Tensor Bt, float xmin, float xmax, int nc){
+    // Problem size
+    const int n_points = points.size(0);
+    const int n_batch = theta.size(0);
+
+    // Allocate output
+    auto output = torch::zeros({n_batch, n_points}, at::kCPU);
+    auto newpoints = output.data_ptr<float>();
+
+    // Convert to pointers
+    float* x = points.data_ptr<float>();
+    for(int i = 0; i < n_batch; i++) { // for all batches
+        
+        // Precompute affine velocity field
+        at::Tensor At = torch_get_affine(Bt, theta.index({i, torch::indexing::Slice()}));
+        float* A = At.data_ptr<float>();
+
+        for(int j = 0; j < n_points; j++) { // for all points
+            newpoints[i*n_points + j] = get_velocity(x[j], A, xmin, xmax, nc);
+        }
+    }
+    return output;
+}
+
+
+// INTEGRATION
+at::Tensor torch_integrate_numeric(at::Tensor points, at::Tensor theta, at::Tensor Bt, float xmin, float xmax, int nc, int nSteps1=10, int nSteps2=10){
+    float t = 1.0;
+
+    // Problem size
+    const int n_points = points.size(0);
+    const int n_batch = theta.size(0);
+
+    // Allocate output
+    auto output = torch::zeros({n_batch, n_points}, at::kCPU);
+    auto newpoints = output.data_ptr<float>();
+
+    // Convert to pointers
+    float* x = points.data_ptr<float>();
+    for(int i = 0; i < n_batch; i++) { // for all batches
+        
+        // Precompute affine velocity field
+        at::Tensor At = torch_get_affine(Bt, theta.index({i, torch::indexing::Slice()}));
+        float* A = At.data_ptr<float>();
+
+        for(int j = 0; j < n_points; j++) { // for all points
+            newpoints[i*n_points + j] = integrate_numeric(x[j], t, A, xmin, xmax, nc, nSteps1, nSteps2);
+        }
+    }
+    return output;
 }
 
 at::Tensor torch_integrate_closed_form(at::Tensor points, at::Tensor theta, at::Tensor Bt, float xmin, float xmax, int nc){
@@ -22,7 +89,7 @@ at::Tensor torch_integrate_closed_form(at::Tensor points, at::Tensor theta, at::
     for(int i = 0; i < n_batch; i++) { // for all batches
 
         // Precompute affine velocity field
-        at::Tensor At = get_affine(Bt, theta.index({i, torch::indexing::Slice()}));
+        at::Tensor At = torch_get_affine(Bt, theta.index({i, torch::indexing::Slice()}));
         float* A = At.data_ptr<float>();
 
         for(int j = 0; j < n_points; j++) { // for all points
@@ -33,33 +100,81 @@ at::Tensor torch_integrate_closed_form(at::Tensor points, at::Tensor theta, at::
 }
 
 
-at::Tensor torch_integrate_numeric(at::Tensor points, at::Tensor theta, at::Tensor Bt, float xmin, float xmax, int nc, int nSteps1=10, int nSteps2=10){
+// DERIVATIVE
+
+// TODO: remove method (two integrations for gradient computation)
+at::Tensor torch_derivative_numeric_old(at::Tensor points, at::Tensor theta, at::Tensor Bt, float xmin, float xmax, int nc, int nSteps1=10, int nSteps2=10, float h=1e-3){
     float t = 1.0;
 
     // Problem size
     const int n_points = points.size(0);
     const int n_batch = theta.size(0);
+    const int d = theta.size(1);
+
+
+
 
     // Allocate output
-    auto output = torch::zeros({n_batch, n_points}, at::kCPU);
-    auto newpoints = output.data_ptr<float>();
+    const int e = 3;
+    // auto output = torch::zeros({n_batch, n_points}, at::kCPU);
+    // auto newpoints = output.data_ptr<float>();
+    auto gradient = torch::zeros({n_batch, n_points, d}, at::kCPU);
+    auto gradpoints = gradient.data_ptr<float>();
+
 
     // Convert to pointers
+    float* B = Bt.data_ptr<float>();
     float* x = points.data_ptr<float>();
-    for(int i = 0; i < n_batch; i++) { // for all batches
-        
-        // Precompute affine velocity field
-        at::Tensor At = get_affine(Bt, theta.index({i, torch::indexing::Slice()}));
-        float* A = At.data_ptr<float>();
+    for(int k = 0; k < d; k++){
+        for(int i = 0; i < n_batch; i++) { // for all batches
+            // Precompute affine velocity field
+            at::Tensor At1 = torch_get_affine(Bt, theta.index({i, torch::indexing::Slice()}));
+            float* A1 = At1.data_ptr<float>();
 
-        for(int j = 0; j < n_points; j++) { // for all points
-            newpoints[i*n_points + j] = integrate_numeric(x[j], t, A, xmin, xmax, nc, nSteps1, nSteps2);
+            // TODO: review this, make it faster
+            auto theta_acc = theta.accessor<float,2>();
+            float current = theta_acc[i][k];
+            theta.index_put_({i, k}, current + h);
+            at::Tensor At2 = torch_get_affine(Bt, theta.index({i, torch::indexing::Slice()}));
+            float* A2 = At2.data_ptr<float>();
+            theta.index_put_({i, k}, current);
+
+            float phi_1, phi_2;
+            for(int j = 0; j < n_points; j++) { // for all points
+                phi_1 = integrate_numeric(x[j], t, A1, xmin, xmax, nc, nSteps1, nSteps2);
+                phi_2 = integrate_numeric(x[j], t, A2, xmin, xmax, nc, nSteps1, nSteps2);
+
+                gradpoints[i*(n_points * d) + j*d + k] = (phi_2 - phi_1)/h;
+            }
         }
     }
-    return output;
+    return gradient;
 }
 
-// TODO: remove method
+// TODO: remove method? maybe not, it is useful for gradient method
+at::Tensor torch_derivative_numeric(at::Tensor points, at::Tensor theta, at::Tensor Bt, float xmin, float xmax, int nc, int nSteps1=10, int nSteps2=10, float h=1e-3){
+    // Problem size
+    const int n_points = points.size(0);
+    const int n_batch = theta.size(0);
+    const int d = theta.size(1);
+
+    auto gradient = torch::zeros({n_batch, n_points, d}, at::kCPU);
+
+    at::Tensor phi_1 =  torch_integrate_numeric(points, theta, Bt, xmin, xmax, nc, nSteps1, nSteps2);
+    // at::Tensor phi_1 =  torch_integrate_closed_form(points, theta, Bt, xmin, xmax, nc);
+    
+    for(int k = 0; k < d; k++){
+        at::Tensor theta2 = theta.clone();
+        at::Tensor row = theta.index({torch::indexing::Slice(), k});
+        theta2.index_put_({torch::indexing::Slice(), k}, row + h);
+        at::Tensor phi_2 =  torch_integrate_numeric(points, theta2, Bt, xmin, xmax, nc, nSteps1, nSteps2);
+        // at::Tensor phi_2 =  torch_integrate_closed_form(points, theta2, Bt, xmin, xmax, nc);
+        gradient.index_put_({torch::indexing::Slice(), torch::indexing::Slice(), k}, (phi_2 - phi_1)/h);
+    }
+    return gradient;
+}
+
+// TODO: remove method (has std::vector)
 at::Tensor torch_derivative_old(at::Tensor points, at::Tensor theta, at::Tensor Bt, float xmin, float xmax, int nc){
     float t = 1.0;
 
@@ -80,7 +195,7 @@ at::Tensor torch_derivative_old(at::Tensor points, at::Tensor theta, at::Tensor 
     for(int i = 0; i < n_batch; i++) { // for all batches
 
         // Precompute affine velocity field
-        at::Tensor At = get_affine(Bt, theta.index({i, torch::indexing::Slice()}));
+        at::Tensor At = torch_get_affine(Bt, theta.index({i, torch::indexing::Slice()}));
         float* A = At.data_ptr<float>();
 
         for(int j = 0; j < n_points; j++) { // for all points
@@ -117,7 +232,7 @@ at::Tensor torch_derivative_closed_form(at::Tensor points, at::Tensor theta, at:
     for(int i = 0; i < n_batch; i++) { // for all batches
 
         // Precompute affine velocity field
-        at::Tensor At = get_affine(Bt, theta.index({i, torch::indexing::Slice()}));
+        at::Tensor At = torch_get_affine(Bt, theta.index({i, torch::indexing::Slice()}));
         float* A = At.data_ptr<float>();
 
         for(int j = 0; j < n_points; j++) { // for all points
@@ -138,102 +253,10 @@ at::Tensor torch_derivative_closed_form(at::Tensor points, at::Tensor theta, at:
     // return output;
 }
 
-at::Tensor torch_derivative_numeric(at::Tensor points, at::Tensor theta, at::Tensor Bt, float xmin, float xmax, int nc, int nSteps1=10, int nSteps2=10, float h=1e-3){
-    // Problem size
-    const int n_points = points.size(0);
-    const int n_batch = theta.size(0);
-    const int d = theta.size(1);
-
-    auto gradient = torch::zeros({n_batch, n_points, d}, at::kCPU);
-
-    at::Tensor phi_1 =  torch_integrate_numeric(points, theta, Bt, xmin, xmax, nc, nSteps1, nSteps2);
-    // at::Tensor phi_1 =  torch_integrate_closed_form(points, theta, Bt, xmin, xmax, nc);
-    
-    for(int k = 0; k < d; k++){
-        at::Tensor theta2 = theta.clone();
-        at::Tensor row = theta.index({torch::indexing::Slice(), k});
-        theta2.index_put_({torch::indexing::Slice(), k}, row + h);
-        at::Tensor phi_2 =  torch_integrate_numeric(points, theta2, Bt, xmin, xmax, nc, nSteps1, nSteps2);
-        // at::Tensor phi_2 =  torch_integrate_closed_form(points, theta2, Bt, xmin, xmax, nc);
-        gradient.index_put_({torch::indexing::Slice(), torch::indexing::Slice(), k}, (phi_2 - phi_1)/h);
-    }
-    return gradient;
-}
-at::Tensor torch_derivative_numeric2(at::Tensor phi_1, at::Tensor points, at::Tensor theta, at::Tensor Bt, float xmin, float xmax, int nc, int nSteps1=10, int nSteps2=10, float h=1e-3){
-    // Problem size
-    const int n_points = points.size(0);
-    const int n_batch = theta.size(0);
-    const int d = theta.size(1);
-
-    auto gradient = torch::zeros({n_batch, n_points, d}, at::kCPU);
-
-    // at::Tensor phi_1 =  torch_integrate_numeric(points, theta, Bt, xmin, xmax, nc, nSteps1, nSteps2);
-    // at::Tensor phi_1 =  torch_integrate_closed_form(points, theta, Bt, xmin, xmax, nc);
-    
-    for(int k = 0; k < d; k++){
-        at::Tensor theta2 = theta.clone();
-        at::Tensor row = theta2.index({torch::indexing::Slice(), k});
-        theta2.index_put_({torch::indexing::Slice(), k}, row + h);
-        at::Tensor phi_2 =  torch_integrate_numeric(points, theta2, Bt, xmin, xmax, nc, nSteps1, nSteps2);
-        // at::Tensor phi_2 =  torch_integrate_closed_form(points, theta2, Bt, xmin, xmax, nc);
-        gradient.index_put_({torch::indexing::Slice(), torch::indexing::Slice(), k}, (phi_2 - phi_1)/h);
-    }
-    return gradient;
-}
 
 
 
-
-at::Tensor torch_derivative_numeric_old(at::Tensor points, at::Tensor theta, at::Tensor Bt, float xmin, float xmax, int nc, int nSteps1=10, int nSteps2=10, float h=1e-3){
-    float t = 1.0;
-
-    // Problem size
-    const int n_points = points.size(0);
-    const int n_batch = theta.size(0);
-    const int d = theta.size(1);
-
-
-
-
-    // Allocate output
-    const int e = 3;
-    // auto output = torch::zeros({n_batch, n_points}, at::kCPU);
-    // auto newpoints = output.data_ptr<float>();
-    auto gradient = torch::zeros({n_batch, n_points, d}, at::kCPU);
-    auto gradpoints = gradient.data_ptr<float>();
-
-
-    // Convert to pointers
-    float* B = Bt.data_ptr<float>();
-    float* x = points.data_ptr<float>();
-    for(int k = 0; k < d; k++){
-        for(int i = 0; i < n_batch; i++) { // for all batches
-            // Precompute affine velocity field
-            at::Tensor At1 = get_affine(Bt, theta.index({i, torch::indexing::Slice()}));
-            float* A1 = At1.data_ptr<float>();
-
-            // TODO: review this, make it faster
-            auto theta_acc = theta.accessor<float,2>();
-            float current = theta_acc[i][k];
-            theta.index_put_({i, k}, current + h);
-            at::Tensor At2 = get_affine(Bt, theta.index({i, torch::indexing::Slice()}));
-            float* A2 = At2.data_ptr<float>();
-            theta.index_put_({i, k}, current);
-
-            float phi_1, phi_2;
-            for(int j = 0; j < n_points; j++) { // for all points
-                phi_1 = integrate_numeric(x[j], t, A1, xmin, xmax, nc, nSteps1, nSteps2);
-                phi_2 = integrate_numeric(x[j], t, A2, xmin, xmax, nc, nSteps1, nSteps2);
-
-                gradpoints[i*(n_points * d) + j*d + k] = (phi_2 - phi_1)/h;
-            }
-        }
-    }
-    return gradient;
-}
-
-
-// SEPARATE derivative into two parts
+// TRANSFORMATION
 at::Tensor torch_integrate_closed_form_trace(at::Tensor points, at::Tensor theta, at::Tensor Bt, float xmin, float xmax, int nc){
     float t = 1.0;
 
@@ -253,7 +276,7 @@ at::Tensor torch_integrate_closed_form_trace(at::Tensor points, at::Tensor theta
     for(int i = 0; i < n_batch; i++) { // for all batches
 
         // Precompute affine velocity field
-        at::Tensor At = get_affine(Bt, theta.index({i, torch::indexing::Slice()}));
+        at::Tensor At = torch_get_affine(Bt, theta.index({i, torch::indexing::Slice()}));
         float* A = At.data_ptr<float>();
 
         for(int j = 0; j < n_points; j++) { // for all points
@@ -288,7 +311,7 @@ at::Tensor torch_derivative_closed_form_trace(at::Tensor output, at::Tensor poin
     for(int i = 0; i < n_batch; i++) { // for all batches
 
         // Precompute affine velocity field
-        at::Tensor At = get_affine(Bt, theta.index({i, torch::indexing::Slice()}));
+        at::Tensor At = torch_get_affine(Bt, theta.index({i, torch::indexing::Slice()}));
         float* A = At.data_ptr<float>();
 
         for(int j = 0; j < n_points; j++) { // for all points
@@ -304,46 +327,30 @@ at::Tensor torch_derivative_closed_form_trace(at::Tensor output, at::Tensor poin
     // return output;
 }
 
-
-
-
-
-at::Tensor get_cell_wrapper(at::Tensor points, const float xmin, const float xmax, const int nc){
-    const int n_points = points.size(0);
-    float* x = points.data_ptr<float>();
-
-    auto output = torch::zeros({n_points}, at::kCPU);
-    auto newpoints = output.data_ptr<float>();
-
-    for(int i = 0; i < n_points; i++) {
-        newpoints[i] = get_cell(x[i], xmin, xmax, nc);
-    }
-    return output;
-}
-
-at::Tensor get_velocity_wrapper(at::Tensor points, at::Tensor theta, at::Tensor Bt, float xmin, float xmax, int nc){
+at::Tensor torch_derivative_numeric_trace(at::Tensor phi_1, at::Tensor points, at::Tensor theta, at::Tensor Bt, float xmin, float xmax, int nc, int nSteps1=10, int nSteps2=10, float h=1e-3){
     // Problem size
     const int n_points = points.size(0);
     const int n_batch = theta.size(0);
+    const int d = theta.size(1);
 
-    // Allocate output
-    auto output = torch::zeros({n_batch, n_points}, at::kCPU);
-    auto newpoints = output.data_ptr<float>();
+    auto gradient = torch::zeros({n_batch, n_points, d}, at::kCPU);
 
-    // Convert to pointers
-    float* x = points.data_ptr<float>();
-    for(int i = 0; i < n_batch; i++) { // for all batches
-        
-        // Precompute affine velocity field
-        at::Tensor At = get_affine(Bt, theta.index({i, torch::indexing::Slice()}));
-        float* A = At.data_ptr<float>();
-
-        for(int j = 0; j < n_points; j++) { // for all points
-            newpoints[i*n_points + j] = get_velocity(x[j], A, xmin, xmax, nc);
-        }
+    // at::Tensor phi_1 =  torch_integrate_numeric(points, theta, Bt, xmin, xmax, nc, nSteps1, nSteps2);
+    // at::Tensor phi_1 =  torch_integrate_closed_form(points, theta, Bt, xmin, xmax, nc);
+    
+    for(int k = 0; k < d; k++){
+        at::Tensor theta2 = theta.clone();
+        at::Tensor row = theta2.index({torch::indexing::Slice(), k});
+        theta2.index_put_({torch::indexing::Slice(), k}, row + h);
+        at::Tensor phi_2 =  torch_integrate_numeric(points, theta2, Bt, xmin, xmax, nc, nSteps1, nSteps2);
+        // at::Tensor phi_2 =  torch_integrate_closed_form(points, theta2, Bt, xmin, xmax, nc);
+        gradient.index_put_({torch::indexing::Slice(), torch::indexing::Slice(), k}, (phi_2 - phi_1)/h);
     }
-    return output;
+    return gradient;
 }
+
+
+
 
 
 
@@ -355,9 +362,9 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.def("integrate_numeric", &torch_integrate_numeric, "Integrate numeric");
     m.def("derivative_closed_form", &torch_derivative_closed_form, "Derivative closed form");
     m.def("derivative_numeric", &torch_derivative_numeric, "Derivative numeric");
-    m.def("derivative_numeric2", &torch_derivative_numeric2, "Derivative numeric2");
-    m.def("get_cell", &get_cell_wrapper, "Get cell");
-    m.def("get_velocity", &get_velocity_wrapper, "Get Velocity");
+    m.def("derivative_numeric_trace", &torch_derivative_numeric_trace, "Derivative numeric2");
+    m.def("get_cell", &torch_get_cell, "Get cell");
+    m.def("get_velocity", &torch_get_velocity, "Get Velocity");
 }
 
 
