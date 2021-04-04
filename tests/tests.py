7bfe7272f523698efe1fb0d95a62339fc5e2686f
imartinezl <inigomlap@gmail.com>
2021-04-04 10:46:05 +0200

    MCMC sampling tests

--- tests/tests.py
+++ tests/tests.py
@@ -8,6 +8,7 @@ import numpy as np
 import torch
 import matplotlib.pyplot as plt
 import cpab
+from tqdm import tqdm 
 
 # %% TEST 
 
@@ -34,7 +35,7 @@ T.visualize_gradient(theta)
 T.visualize_gradient(theta, mode="numeric")
 
 
-# %% OPTIMIZATION
+# %% OPTIMIZATION BY GRADIENT
 
 tess_size = 5
 backend = "pytorch" # ["pytorch", "numpy"]
@@ -49,18 +50,17 @@ T.params.use_slow = use_slow
 
 grid = T.uniform_meshgrid(outsize)
 
-# theta_1 = T.identity(batch_size, epsilon=0)
 theta_1 = T.sample_transformation(batch_size)
 grid_t1 = T.transform_grid(grid, theta_1)
 
-theta_2 = T.sample_transformation(batch_size)
-theta_2.requires_grad = True
+theta_2 = torch.autograd.Variable(T.sample_transformation(batch_size), requires_grad=True)
 
 lr = 1e-1
 optimizer = torch.optim.Adam([theta_2], lr=lr)
 
 loss_values = []
 maxiter = 50
+pb = tqdm(desc='Alignment of samples', unit='iters', total=maxiter)
 for i in range(maxiter):
     optimizer.zero_grad()
     grid_t2 = T.transform_grid(grid, theta_2, mode="closed_form")
@@ -69,7 +69,9 @@ for i in range(maxiter):
     optimizer.step()
 
     loss_values.append(loss.item())
-
+    pb.update()
+    pb.set_postfix({'loss': loss.item()})
+pb.close()
 
 plt.figure()
 plt.plot(loss_values)
@@ -84,6 +86,67 @@ plt.figure()
 plt.plot(grid_t1.t() - grid_t2.detach().t())
 plt.axhline(color="black", ls="dashed")
 # theta_1, theta_2
+
+# %% OPTIMIZATION BY MCMC SAMPLING
+
+tess_size = 5
+backend = "pytorch" # ["pytorch", "numpy"]
+device = "cpu" # ["cpu", "gpu"]
+zero_boundary = True
+use_slow = False
+outsize = 100
+batch_size = 1
+
+T = cpab.Cpab(tess_size, backend, device, zero_boundary)
+T.params.use_slow = use_slow
+
+grid = T.uniform_meshgrid(outsize)
+
+theta_ref = T.sample_transformation(batch_size)
+grid_ref = T.transform_grid(grid, theta_ref)
+
+current_sample = T.sample_transformation(batch_size)
+grid_t = T.transform_grid(grid, current_sample)
+
+current_error = np.linalg.norm(grid_t - grid_ref)
+accept_ratio = 0
+
+samples = []
+maxiter = 1500
+pb = tqdm(desc='Alignment of samples', unit='samples', total=maxiter)
+for i in range(maxiter):
+    # Sample and transform 
+    theta = T.sample_transformation(batch_size, mean=current_sample.flatten())
+    grid_t = T.transform_grid(grid, theta, mode="closed_form")
+
+    # Calculate new error
+    new_error = np.linalg.norm(grid_t - grid_ref)
+
+    samples.append( T.backend.tonumpy(theta[0]))
+    
+    if new_error < current_error:
+        current_sample = theta
+        current_error = new_error
+        accept_ratio += 1
+    pb.update()
+print('Acceptence ratio: ', accept_ratio / maxiter * 100, '%')
+pb.close()
+
+# samples = np.array(samples)
+# for i in range(len(theta[0])):
+#     plt.figure()
+#     plt.hist(samples[:,i])
+#     plt.axvline(theta_ref[0][i], c="red", ls="dashed")
+
+
+theta = np.mean(samples, axis=0)[np.newaxis,:]
+grid_t = T.transform_grid(grid, T.backend.to(theta), mode="closed_form")
+
+plt.figure()
+plt.plot(grid, grid_ref[0])
+plt.plot(grid, grid_t[0])
+
+
 
 
 # %% TRANSFORM DATA
