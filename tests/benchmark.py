51f23a7f94f8e151a2686f8e5667aea6631dfb6f
imartinezl <inigomlap@gmail.com>
2021-04-07 19:31:03 +0200

    Benchmark: forward and backward  + boxplots

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -1,7 +1,8 @@
 # %%
 
 import sys
-sys.path.insert(0,'..')
+
+sys.path.insert(0, "..")
 
 import time
 import timeit
@@ -15,8 +16,8 @@ import torch.utils.benchmark as benchmark
 
 # %% SETUP
 tess_size = 50
-backend = "pytorch" # ["pytorch", "numpy"]
-device = "cpu" # ["cpu", "gpu"]
+backend = "pytorch"  # ["pytorch", "numpy"]
+device = "cpu"  # ["cpu", "gpu"]
 zero_boundary = True
 use_slow = False
 outsize = 100
@@ -38,6 +39,7 @@ plt.plot(grid_t.T)
 print(1)
 # %%
 import yep
+
 torch.set_num_threads(1)
 
 theta_grad = torch.autograd.Variable(theta, requires_grad=True)
@@ -62,13 +64,17 @@ print("Time: ", np.mean(timing) / n, "+-", np.std(timing) / np.sqrt(n))
 # %% CPROFILE
 
 import cProfile
-cProfile.run('''
+
+cProfile.run(
+    """
 theta_grad = torch.autograd.Variable(theta, requires_grad=True)
 for i in range(100): 
     grid_t = T.transform_grid(grid, theta_grad, method)
     loss = torch.norm(grid_t)
     loss.backward()
-''', sort="cumtime")
+""",
+    sort="cumtime",
+)
 # %% PYTORCH PROFILER
 
 with profiler.profile(with_stack=True, profile_memory=True) as prof:
@@ -79,88 +85,98 @@ print(prof.key_averages().table(sort_by="self_cpu_time_total", row_limit=50))
 
 # %%
 t0 = benchmark.Timer(
-    stmt="T.transform_grid(grid, theta)",
-    globals={"T":T, "grid":grid, "theta": theta}
+    stmt="T.transform_grid(grid, theta)", globals={"T": T, "grid": grid, "theta": theta}
 )
 t0.timeit(1)
 
+# %% snakeviz
+# %prun -D program.prof T.transform_grid(grid, theta)
+
 # %%
+
 from itertools import product
-import pickle
+
 results = []
 
-# tess_size_array = [3,5,10,20,50,100]
-# outsize_array = [10,20,50,100,200]
-# batch_size_array = [1,2,5,10,20,100]
-# num_threads_array = [1,2,4]
-tess_size_array = [100]
-outsize_array = [200]
-batch_size_array = [100]
-num_threads_array = [1]
+num_threads_arr = [1, 2, 4]
 
-for tess_size, outsize, batch_size in product(tess_size_array, outsize_array, batch_size_array):
-    backend = "pytorch"
-    T = cpab.Cpab(tess_size, backend, zero_boundary=True, device="cpu")
+backend_arr = ["pytorch"] # ["pytorch", "numpy"]
+device_arr = ["cpu"] # ["cpu", "gpu"]
+method_arr = ["closed_form", "numeric"] # ["closed_form", "numeric"]
+use_slow_arr = [False, True] # [True, False]
+zero_boundary_arr = [True, False] # [True, False]
+
+tess_size_arr = [50]
+outsize_arr = [100]
+batch_size_arr = [20]
+
+for (
+    backend,
+    device,
+    method,
+    use_slow,
+    zero_boundary,
+    tess_size,
+    outsize,
+    batch_size,
+) in product(
+    backend_arr,
+    device_arr,
+    method_arr,
+    use_slow_arr,
+    zero_boundary_arr,
+    tess_size_arr,
+    outsize_arr,
+    batch_size_arr,
+):
+    # SETUP
+    T = cpab.Cpab(tess_size, backend, device, zero_boundary)
+    T.params.use_slow = use_slow
+
     grid = T.uniform_meshgrid(outsize)
     theta = T.identity(batch_size, epsilon=1)
 
-    T.transform_grid(grid, theta)
-
-    label = "Transform Grid"
-    sub_label = f'[{tess_size}, {outsize}, {batch_size}]'
-
+    label = "CPAB: backend, device, method, use_slow, zero_boundary, tess_size, outsize, batch_size"
+    # sub_label = f"[{backend}, {device}, {method}, {'slow' if use_slow else 'fast'}, {'zero_boundary' if zero_boundary else 'no_zero_boundary'}, {tess_size}, {outsize}, {batch_size}]"
+    sub_label = f"[{backend}, {device}, {method}, {use_slow}, {zero_boundary}, {tess_size}, {outsize}, {batch_size}]"
     print(sub_label)
+    for num_threads in num_threads_arr:
+        repetitions = 10
 
-    repetitions = 1
-
-    for num_threads in num_threads_array:
-
-
-        T.params.use_slow = False
+        # FORWARD
         t0 = benchmark.Timer(
-            stmt="T.transform_grid(grid, theta, method='closed_form')",
-            globals={"T": T, "grid": grid, "theta": theta},
+            stmt=
+            """
+            grid_t = T.transform_grid(grid, theta, method)
+            """,
+            globals={"T": T, "grid": grid, "theta": theta, "method": method},
             num_threads=num_threads,
             label=label,
             sub_label=sub_label,
-            description="CPU / FAST / CLOSED FORM"
+            description="Forward",
         )
-        results.append(t0.timeit(repetitions))
+        # results.append(t0.timeit(repetitions))
+        # results.append(t0.blocked_autorange(min_run_time=0.5))
+        results.append(t0.adaptive_autorange())
 
-
-        T.params.use_slow = True
+        # BACKWARD
         t1 = benchmark.Timer(
-            stmt="T.transform_grid(grid, theta, method='closed_form')",
-            globals={"T": T, "grid": grid, "theta": theta},
+            stmt=
+            """
+            theta_grad = torch.autograd.Variable(theta, requires_grad=True)
+            grid_t = T.transform_grid(grid, theta_grad, method)
+            loss = torch.norm(grid_t)
+            loss.backward()            
+            """,
+            globals={"T": T, "grid": grid, "theta": theta, "method": method},
             num_threads=num_threads,
             label=label,
             sub_label=sub_label,
-            description="CPU / SLOW / CLOSED FORM"
+            description="Backward",
         )
-        results.append(t1.timeit(repetitions))
-
-        T.params.use_slow = False
-        t2 = benchmark.Timer(
-            stmt="T.transform_grid(grid, theta, method='numeric')",
-            globals={"T": T, "grid": grid, "theta": theta},
-            num_threads=num_threads,
-            label=label,
-            sub_label=sub_label,
-            description="CPU / FAST / NUMERIC"
-        )
-        results.append(t2.timeit(repetitions))
-
-        T.params.use_slow = True
-        t3 = benchmark.Timer(
-            stmt="T.transform_grid(grid, theta, method='numeric')",
-            globals={"T": T, "grid": grid, "theta": theta},
-            num_threads=num_threads,
-            label=label,
-            sub_label=sub_label,
-            description="CPU / SLOW / NUMERIC"
-        )
-        results.append(t3.timeit(repetitions))
-    
+        # results.append(t1.timeit(repetitions))
+        # results.append(t1.blocked_autorange(min_run_time=0.5))
+        results.append(t1.adaptive_autorange())
 
 
 # %%
@@ -169,6 +185,65 @@ compare.trim_significant_figures()
 compare.colorize()
 compare.print()
 
+# %%
+import seaborn as sns
+import pandas as pd
 
-# %% snakeviz
-# %prun -D program.prof T.transform_grid(grid, theta)
+df = [
+    pd.DataFrame({
+        'experiment': t.as_row_name, 
+        'description': t.task_spec.description,
+        'threads': t.task_spec.num_threads,
+        'time': t.raw_times})
+    for t in results
+]
+df = pd.concat(df, ignore_index=True)
+df['experiment_id'] = df.groupby('experiment', sort=False).ngroup().apply(str)
+
+n = pd.unique(df.experiment_id)
+exps = pd.unique(df.experiment)
+caption = '\n'.join([k + ": " + exps[int(k)] for k in n])
+
+header = ['Backend', 'Device', 'Method', 'Speed', 'Boundary', 'Tess Size', 'Grid Size', 'Batch Size']
+cell_text = [e.replace('[','').replace(']','').split(', ') for e in exps]
+
+vlen = np.vectorize(len)
+w = np.max(vlen(cell_text + [header]), axis=0)
+
+# %%
+import matplotlib
+
+
+with sns.axes_style("whitegrid"):
+    g = sns.catplot(
+        x="time", y="experiment_id", 
+        hue="threads", col="description",
+        data=df, kind="box", ci=None, sharex=True,
+        fliersize=2, linewidth=1, width=0.75)
+    sns.despine(top=False, right=False, left=False, bottom=False)
+    plt.xticks(np.logspace(-10,-1, num=10))
+    # plt.figtext(0, -0.1, caption, wrap=True, 
+    #     verticalalignment='top', horizontalalignment='left', fontsize=10)
+
+    table = plt.table(
+        cellText=cell_text, 
+        rowLabels=n, 
+        colLabels=header, 
+        colWidths = w,
+        cellLoc='center',
+        loc='bottom',
+        # fontsize=50
+        bbox=[-1.0,-0.5, 1.2, 0.35]
+        )
+    table.auto_set_font_size(False)
+    table.set_fontsize(8)
+    # table.auto_set_column_width(n)
+    # table.scale(1, 1)
+
+    for ax in g.axes[0]:
+        ax.set_xscale('log')
+        ax.grid(axis="x", which="minor", ls="--", c='gray', alpha=0.2)
+
+plt.savefig('example.png')
+
+# %%
