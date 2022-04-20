Gradient space: bug fix

cpab/backend/numpy/transformer.py
@@ -367,7 +367,7 @@ def derivative_space_closed_form(x, theta, params, time=1.0):
     c = get_cell(x, params)
     valid = c == cm
     # dpsi_dx only on first valid cell
-    dpsi_dx[valid] = derivative_phi_x(x, t, theta, params)[valid]
+    dpsi_dx[valid] = derivative_psi_x(x, t, theta, params)[valid]
     # dthit_dx only on first non valid cell
     dthit_dx[~valid] = derivative_thit_x(x, t, theta, params)[~valid]
 
@@ -381,7 +381,7 @@ def derivative_space_closed_form(x, theta, params, time=1.0):
         c = c + step
     
     # dpsi_dtime at last cell
-    dpsi_dtime = derivative_phi_t(xm, tm, theta, params)
+    dpsi_dtime = derivative_psi_t(xm, tm, theta, params)
     dphi_dx = dpsi_dx + dpsi_dtime * dthit_dx
 
     dphi_dx = dphi_dx.reshape(n_batch, n_points)
@@ -397,7 +397,7 @@ def derivative_thit_x(x, t, theta, params):
 
     return 1.0 / (a*x + b)
 
-def derivative_phi_x(x, t, theta, params):
+def derivative_psi_x(x, t, theta, params):
     A, r = get_affine(x, theta, params)
     c = get_cell(x, params)
     a = A[r, c, 0]
@@ -405,7 +405,7 @@ def derivative_phi_x(x, t, theta, params):
 
     return np.exp(t * a)
 
-def derivative_phi_t(x, t, theta, params):
+def derivative_psi_t(x, t, theta, params):
     A, r = get_affine(x, theta, params)
     c = get_cell(x, params)
     a = A[r, c, 0]